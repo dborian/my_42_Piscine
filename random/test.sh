446951b5feb9d7a233accef12649c715165b1a5e
dborian <edmedorian@gmail.com>
2024-12-02 23:17:33 +0100

    public

--- random/test.sh
+++
@@ -1,8 +0,0 @@
-cp $@ ~/goinfre
-name=$(echo $@ | sed 's#.*/##')
-sed -i '1,11d' ~/goinfre/$name
-sed -i "s/\/\///g" ~/goinfre/$name
-sed -i "s/\/\*//g" ~/goinfre/$name
-sed -i "s/\*\///g" ~/goinfre/$name
-cc -g3 -lbsd -Wall -Wextra -Werror ~/goinfre/$name
-rm ~/goinfre/$name
